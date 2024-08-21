b641508ad9071b88825bbdb5713290e55abf975f
nachoplus <mas.ignacio@gmail.com>
2024-08-22 01:41:33 +0200

    correccion tipo IVA en las tablas de coeficientes

--- pipupload.sh
+++ pipupload.sh
@@ -1,6 +1,6 @@
-tagname='0.1.2'
+tagname='0.1.3'
 git tag --delete ${tagname}
-git tag ${tagname}   -m "actualizacion de las tablas de coeficientes"
+git tag ${tagname}   -m "correccion del tipo de IVA  de las tablas de coeficientes"
 git push origin :refs/tags/${tagname}
 git push --tags origin main
 rm dist/*
